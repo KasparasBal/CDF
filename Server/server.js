eb65fdbaaadbb0ed913fcc780c651016425d62b5
KasparasBal <Kasparasbalys@gmail.com>
2022-07-15 12:44:40 +0300

    Commit: 3 Login endpoint

--- Server/server.js
+++ Server/server.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
 //middleware
 const dbConnect = require("./db/dbConnect");
+const auth = require("./auth");
 
 //models
 
@@ -62,6 +64,72 @@ app.post("/register", (request, response) => {
         e,
       });
     });
+});
+
+//login
+
+// login
+app.post("/login", (request, response) => {
+  // check if email exists
+  User.findOne({ email: request.body.email })
+
+    // if email exists
+    .then((user) => {
+      // compare  password  and  hashed password
+      bcrypt
+        .compare(request.body.password, user.password)
+
+        .then((passwordCheck) => {
+          // passwords match?
+          if (!passwordCheck) {
+            return response.status(400).send({
+              message: "Passwords does not match",
+              error,
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          // res success
+          response.status(200).send({
+            message: "Login Successful",
+            email: user.email,
+            token,
+          });
+        })
+        // err - passwords don't match
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords do not match",
+            error,
+          });
+        });
+    })
+    // err -email not found
+    .catch((e) => {
+      response.status(404).send({
+        message: "Email not found",
+        e,
+      });
+    });
+});
+
+// not logged in
+app.get("/free-endpoint", (request, response) => {
+  response.json({ message: "Everyone is welcome" });
+});
+
+// logged in
+app.get("/auth-endpoint", auth, (request, response) => {
+  response.json({ message: "Hello logged in user" });
 });
 
 const port = process.env.PORT || 8000;
